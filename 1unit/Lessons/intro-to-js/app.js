e5a7ec32cc09c333ebfcf5eed30961b405ff6f37
MShubar <pokeeeeepow@gmail.com>
2024-09-17 17:07:51 +0300

    Javascript finished

---
+++ 1unit/Lessons/intro-to-js/app.js
@@ -0,0 +1,27 @@
+// this is a single line comment (control+/)
+/* this is a multiline comment (alt+shift+A) */
+console.log('Hello Mohsen') //console Logging prints a text
+
+//this is a constant Variables
+let favSnack
+const myName = 'Mohsen'
+console.log(myName) //this prints your variables
+
+favSnack = 'pizza' //this is a changing Variables
+favSnack = 'Samboosa'
+console.log(favSnack)
+
+let myNumber = 4
+let myOtherNumber = '4'
+console.log(myNumber * 3)
+
+console.log(true)
+
+let myChoice = null
+console.log(myChoice)
+
+let greeting = `Hello ${myName}`
+console.log(greeting)
+
+let studentName = 'Hadi'
+console.log(`Dear ${studentName} Good day to you`)
---
+++ 1unit/Lessons/js-control-flow/app.js
@@ -0,0 +1,10 @@
+let color = 'green'
+if (color === 'green') {
+  console.log('Go')
+} else if (color === 'yellow') {
+  console.log('Slow')
+} else if (color === 'red') {
+  console.log('Stop')
+} else {
+  console.log('Whatever')
+}
