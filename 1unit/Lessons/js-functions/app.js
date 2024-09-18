7e68fb65153783a5257145609faa9176815de7f0
MShubar <pokeeeeepow@gmail.com>
2024-09-18 15:18:51 +0300

    Second commit

---
+++ 1unit/Lessons/js-functions/app.js
@@ -0,0 +1,21 @@
+// function greeting(myName, myAge) {
+//   console.log(`Hi my name is ${myName} I am ${myAge} years old`)
+// }
+// greeting('Mohsen', '23')
+
+// function computeArea(height, width) {
+//   let result = height * width
+//   return result
+// }
+// const result = computeArea(5, 25)
+// console.log(`The area of a rectangle with a width of ${result} `)
+
+// const computeArea(height, width) => {
+//     let result = height * width
+//     return result;
+// }
+
+// const square = (num) => {
+//   return num
+// }
+// square(4)
